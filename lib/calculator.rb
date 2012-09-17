82d6fdedfc1bf49b500e63fb6dbe6da83245dc90
Andrea Schiavini <andrea.schiavini@gmail.com>
2012-09-17 20:27:04 +0200

    Median

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -5,4 +5,14 @@ class Calculator
     collection.inject(0.0) {|sum, x| sum += x} / collection.size
   end
 
+  def median collection
+    return nil if collection.empty?
+    sorted = collection.sort()
+    if (sorted.size.modulo(2) == 0)
+      ((sorted[(sorted.size/2)-1]+sorted[(sorted.size/2)])/2.0).to_f
+    else
+      sorted[sorted.size/2].to_f
+    end
+  end
+
 end
--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -14,5 +14,19 @@ describe Calculator do
     end
   end
 
+  context "Calculating median" do
+    it "returns nil when collection is empty" do
+      Calculator.new.median([]).should be_nil
+    end
+    it "returns same value when only one element" do
+      Calculator.new.median([7]).should eql(7.0)
+    end
+    it "returns the right median value for a collection with an odd elements count" do
+      Calculator.new.median([50, 7, 13]).should eql(13.0)
+    end
+    it "returns the right median value for a collection with an even elements count" do
+      Calculator.new.median([50, 7, 13, 22]).should eql( (22 + 13) / 2.0)
+    end
+  end
 
 end
