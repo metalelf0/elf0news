1d0e0eba5f840ddc2d15eda621dd3560900b9f6e
Andrea Schiavini <andrea.schiavini@gmail.com>
2012-09-17 19:21:21 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metalelf0News::Application.config.secret_token = 'e17ed40047889332a117fbe82e2add766e19b0616566cd52bd6f567a367b3bbe65dd7ccffe0c12fdb1208bf689dd3720d7819370563a866023b190fe26899334'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Metalelf0News::Application.config.session_store :cookie_store, :key => '_metalelf0_news_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Metalelf0News::Application.config.session_store :active_record_store
